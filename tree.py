dbec0e6ee98fdf9c7eea95eea12e7a19a4085c93
Brent Havers <brenthavers@gmail.com>
2021-12-23 23:08:47 -0500

    feat: add insert function to Tree class

--- tree.py
+++ tree.py
@@ -1,5 +1,17 @@
+from node import Node
+
 class Tree:
 
     def __init__(self):
         self.root = None
         self.size = 0
+
+    def insert(self, node: Node, value):
+        if node is None:
+            node = Node(value)
+        else:
+            if node.data > value:
+                self.insert(node.right_child, value)
+            else:
+                self.insert(node.left_child, value)
+        return
